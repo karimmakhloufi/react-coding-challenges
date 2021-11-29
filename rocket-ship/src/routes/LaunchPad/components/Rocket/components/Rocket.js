978428577bb42453bc79361bde1be2dadc0ba157
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-11-29 11:19:24 +0100

    Memoized functional rocket to prevent take off

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,18 +1,18 @@
-import React, { useState, Component } from 'react';
-import RocketCore from './RocketCore';
+import React, { useState, Component } from "react";
+import RocketCore from "./RocketCore";
 
-export function FunctionalRocket() {
+export const FunctionalRocket = React.memo(function () {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
-}
+});
 
 export class ClassRocket extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      initialLaunchTime: Date.now()
+      initialLaunchTime: Date.now(),
     };
   }
 
